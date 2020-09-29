87482a0480bdf39a97117baa375c79780d31a909
Ilyas Ulgur <ulgur@yahoo.com>
2020-09-29 21:23:01 +0200

    Initial commit

---
+++ collec.java
@@ -0,0 +1,97 @@
+Loosely Coupled : Animal bird = new Bird();
+Tigtly Coupled : Bird bird = new Bird();
+
+Coupling 
+
+ArrayList - go to index 450 and remove it
+
+10
+20
+30
+40
+50
+....
+...
+...
+
+
+..
+1000000 .... 500 (millionth of the data)
+
+
+
+Multi Thread.sleep
+
+who is sleeping?
+
+
+Data Structure and Algorithm
+
+
+Java
+
+Core Part -> variables, operators, slection statements, loops, String methods
+OOP -> Class-Objects, Encapsulations, Inheritance, Abstructoion, Polymorphism
+
+Exception Handling
+Collections -> Arrays, List, Set, Map
+
+Java8 Features -> Lambda, Streams
+
+Multi Threading
+
+
+SDET (6-7)
+Developer (12-13)
+Data Science (12-15) -> Master, PhD
+DevOps -> Experience
+
+Scrum Master
+Product Owner
+Project Manager
+
+CyberSECURITY
+
+SDET (6-7)
+	Lead
+	Developer (12-15)
+
+
+Employee Table
+
+key				Value:
+Employee ID 	First Name 	Last Name
+1				Ozzy
+2				Ozzy
+3				Tom
+4				Mike
+5
+6
+7
+
+
+Generics
+
+www.qa.amazon.com
+www.uat.amazon.com
+www.amazon.com
+
+TC1 TC2 TC3
+
+[
+{ Salary=100000, 
+  EmpID=123, 
+  JobTitle=SDET, 
+  EmpName=John
+}, 
+{ Salary=80000, 
+  EmpID=123, 
+  JobTitle=Developer, 
+  EmpName=Mike
+}
+]
+
+JSON
+
+key values in JSON
+
